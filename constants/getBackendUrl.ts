19c0f74ec8c1869c20398b52c8acb359086faa8f
Tayyab Sajid <tayyabsajidq41321@gmail.com>
2025-05-31 10:08:33 +0400

    afd

--- constants/getBackendUrl.ts
+++ constants/getBackendUrl.ts
@@ -1,8 +1,5 @@
 const getBackendUrl = async (): Promise<string> => {
-	const response = await fetch("https://cosmic-clank.github.io/ieeebackendurl/");
-	const text = await response.text();
-	console.log("Backend URL:", text); // Log the full URL for debugging
-	return text;
+	return "192.168.137.1:8000";
 };
 
 export default getBackendUrl;
